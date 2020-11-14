2c39949791411f8f40c86862b3d703f9de3942a4
Mahfuz Ali <mahfuz.ali@hotmail.co.uk>
2020-11-14 14:01:04 +0000

    Initial project setup

---
+++ tests/ECommerce.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ECommerce.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
